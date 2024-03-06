d5434f91da57f399428f48e78fe64ee945f326d5
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2024-03-06 01:04:35 -0600

    Mitad de diseño

--- resources/js/components/global/AppBar.vue
+++ resources/js/components/global/AppBar.vue
@@ -60,38 +60,6 @@ const logOut = async () => {
           color="wine"
           class="mr-3"
           >REGISTRATE</v-btn>
-          <!-- <v-menu transition="scale-transition">
-                <template v-slot:activator="{ props }">
-                  <v-icon color="grey_dark" v-bind="props">
-                    {{ is_dark === false ?  "mdi-weather-sunny" : "mdi-moon-waning-crescent" }}
-                  </v-icon>
-                </template>
-                <v-list :class="is_dark === false ? 'bg-white' : 'bg-background_nav'">
-                  <v-list-item v-model="selectedDark" @click="setTheme" v-if="is_dark === false">
-                    <v-list-item-title class="text-grey_dark"> <v-icon color="grey_dark">mdi-moon-waning-crescent</v-icon> {{ DarkMode }}</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item v-model="selectedLight" @click="setThemeLight" v-else>
-                    <v-list-item-title class="text-grey_dark"> <v-icon color="grey_dark">mdi-weather-sunny</v-icon> {{ LightMode }}</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-          </v-menu>
-          <v-btn icon class="mr-3">
-            <v-badge color="fail" class="py-2">
-              <v-icon
-                variant="text"
-                class="text-grey"
-                @click.stop="toggleDrawer"
-                v-html="$Feather.icons['bell'].toSvg()"
-              >
-              </v-icon>
-            </v-badge>
-          </v-btn>
-          <v-avatar>
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/john.jpg"
-              alt="John"
-            ></v-img>
-          </v-avatar> -->
         </template>
       </v-app-bar>
 </template>
--- resources/js/view/Inicio.vue
+++ resources/js/view/Inicio.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-carousel hide-delimiters cycle :interval="interval" class="custom-carousel">
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-      cover
-    >
+  <v-carousel
+    hide-delimiters
+    cycle
+    :interval="interval"
+    class="custom-carousel"
+  >
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
       <div class="overlay">
         <div class="content">
           <h1 class="font-weight-bold mb-0">
             Sistema Estatal para el Registro de
           </h1>
-          <h1 class="font-weight-bold mb-3">Cédula Estatal e Imagen Turística</h1>
-          <v-btn color="wine" variant="flat" rounded="0" class="pl-15 pr-15">Registrate</v-btn>
+          <h1 class="font-weight-bold mb-3">
+            Cédula Estatal e Imagen Turística
+          </h1>
+          <v-btn color="wine" variant="flat" rounded="0" class="pl-15 pr-15"
+            >Registrate</v-btn
+          >
         </div>
       </div>
     </v-carousel-item>
@@ -98,11 +102,125 @@
             width="350"
             @click="toggle"
           >
-          <img :src="n.src"  height="250"
-            width="350" alt="">
+            <img :src="n.src" height="250" width="350" alt="" />
           </v-card>
         </v-slide-group-item>
       </v-slide-group>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12">
+      <v-carousel
+        hide-delimiters
+        cycle
+        :interval="interval"
+        class="custom-carousel"
+      >
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+          cover
+        >
+          <v-row class="overlay">
+            <v-col cols="12" class="content">
+              <h2>ho</h2>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" class="mt-15">
+      <h1 class="text-center font-weight-bold">Programas de certificación</h1>
+      <h1 class="text-center font-weight-bold">y capacitación</h1>
+    </v-col>
+    <v-col cols="12" sm="4" lg="4">
+      <v-card class="mx-auto my-12" max-width="374" rounded="0" variant="flat">
+        <v-img
+          height="250"
+          src="https://s3-alpha-sig.figma.com/img/4ebf/8772/042d3a76f3cb29cab6c945052074b2a4?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BOBU2NgSW24WdwRTkPWo4pnQJwlrcugt6dzuXu3s3f7GEAz-4KXcIffsW7kDwKaF9Td0noObDcundgEWVis-D-IXcceNH5ZsKxGOE0GvlfIaNs2hO4pBEti7bOC0UOiiMDK1exCjl~Pb0I8mkFaT5iBvc58DqPhC2v6suISO95vXrjxV-DT7acz9CCbWlVy2bKowxZjm4Wd-z3ZvAtUrQPrziEkK-XNkLSnor-W694~HPo8tMXP-WfGXu4w35oFVCu4yhI~lz5wo9S83yi1N4v3BXEK9ZaQEM5SqXpi9iURr6cHAh-X01IHHG92lhpZD8p-9FGmHMRuSEmrfUoBDdA__"
+          cover
+        ></v-img>
+        <v-card-text class="px-0">
+          <v-row class="px-0 justify-space-between">
+            <v-col cols="12">
+              <v-chip color="brown" rounded="0">
+                <span class="font-weight-bold">HOTELEROS</span>
+              </v-chip>
+              <span class="font-weight-bold px-5">4 Lecciones</span>
+              <h5 class="font-weight-bold my-5 text-dark">Atención a personas con discapacidad</h5>
+              <p class="word-break">Identifica el trato correcto a las diferentes discapacidades que puede presentar un huésped.</p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="px-0">
+          <v-btn class="px-0 text-dark">Ver más <v-icon class="text-dark">mdi-chevron-right</v-icon></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4" lg="4">
+      <v-card class="mx-auto my-12" rounded="0" max-width="374" variant="flat">
+        <v-img
+          height="250"
+          src="https://s3-alpha-sig.figma.com/img/7322/dd9a/5929a7a55ac770d19acce621f0a43e96?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WbaNCU04kl7ZOE~ufmQmjitRqXi1xOE0APmZz5PvaCjBAqHKXiIkzxxuIoPKiKo-ix0GF9DFt6LWZVe7wqciZU5GUxKsINhOxhmyneFtsIfFttv0b4gkhgx2eQRXUEflUnA88SlzETNFSlLCEyaRFR51lqXIr5xei6yndwmZe6O2cLHfhy-j6Y5T7yOJazXfJpHMhJgVUTku04yWmh07xnPkldn4uoxpuIy76YcEl0MojBVHHWfrGcnvapWwQHKpsgOGUFtQSjRDyHoLJm6HCqlC46Qfb8o1pBNewL4xVM3kzr2lPJnno0eOTg0~CQq9snT-rpg4sGw~IwJAWk25gA__"
+          cover
+        ></v-img>
+        <v-card-text class="px-0">
+          <v-row class="px-0 justify-space-between">
+            <v-col cols="12">
+              <v-chip color="brown" rounded="0">
+                <span class="font-weight-bold">RESTAURANTES</span>
+              </v-chip>
+              <span class="font-weight-bold px-5">8 Lecciones</span>
+              <h5 class="font-weight-bold my-5 text-dark"  style="word-break: break-all;">Atención a personas con discapacidad</h5>
+              <p class="text-justify">Este curso está diseñado para potenciar el desarrollo integral de habilidades blandas, proporcionando a los participantes las herramientas necesarias para destacar en entornos profesionales y personales.</p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="px-0">
+          <v-btn class="px-0 text-dark">Ver más <v-icon class="text-dark">mdi-chevron-right</v-icon></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4" lg="4">
+      <v-card class="mx-auto my-12" max-width="374" rounded="0" variant="flat">
+        <v-img
+          height="250"
+          src="https://s3-alpha-sig.figma.com/img/3845/026a/e88f9f40c40878d335ee7148e9b49307?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IhiChp2lV6taUqo2gfL3~ISNHO0QcSeHZ6d1chCQ3oGWag7mGDgBwHvJjRoLPfAfXvU5DU5yCz57mFQ9Lqk33vdFxyqitX621wML5VSlkRAk5W6xVHhtPl8d~3yda2Pg8svjXmrMiAI225F0MPynE8trSeLYzu62HqEO00777AL8M1Z5jfO7CLC9UHHeUNdBIIwDAfu3Jx8X2HNiGSg8N462ZHjsXLXqkdc3IO-tJvF-mEjl4~hGFkmp878QJfeu1Bqiwjtv~ZLF5jrNSk2uwtsnSGe-GErIcmBZnJQpJGsWWMYKOjj~GvMPbZbDP9UGgUcv0GYI0GYLovVXjy-Bww__"
+          cover
+        ></v-img>
+        <v-card-text class="px-0">
+          <v-row class="px-0 justify-space-between">
+            <v-col cols="12">
+              <v-chip color="brown" rounded="0">
+                <span class="font-weight-bold">GENERAL</span>
+              </v-chip>
+              <span class="font-weight-bold px-5">10 Lecciones</span>
+              <h5 class="font-weight-bold my-5 text-dark"  style="word-break: break-all;">Atención a personas con discapacidad</h5>
+              <p class="text-justify">El trabajo de un anfitrión conlleva mucho más que sólo la interacción con un huésped. Es importante que generes una estancia agradable y brindes todos los servicios e información que el huésped solicite.</p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="px-0">
+          <v-btn class="px-0 text-dark">Ver más <v-icon class="text-dark">mdi-chevron-right</v-icon></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" class="d-flex justify-center">
+      <v-btn class="text-dark" rounded="0" variant="outlined">
+        Ver todos los programas
+        <v-icon right>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
+  <v-row class="bg-background">
+    <v-col cols="12">
+      <h5 class="text-dark text-center mt-10">Últimos proveedores turísticos registrados en nuestra plataforma</h5>
+      <img class="mt-15 mb-15 ml-15" src="../../../public/img/Content.png"/>
     </v-col>
   </v-row>
 </template>
@@ -111,21 +229,20 @@
 import { ref } from "vue";
 const model = ref(null);
 const interval = ref(3000);
-const items = ref( 
-          [
-          {
-            src: 'https://cdn.pixabay.com/photo/2021/02/01/12/28/fisherman-5970480_1280.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2019/06/29/04/00/fisherman-4305368_1280.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2019/11/04/18/01/fisherman-4601744_1280.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2018/01/05/02/47/fishing-3062034_1280.jpg',
-          },
-        ],)
+const items = ref([
+  {
+    src: "https://cdn.pixabay.com/photo/2021/02/01/12/28/fisherman-5970480_1280.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2019/06/29/04/00/fisherman-4305368_1280.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2019/11/04/18/01/fisherman-4601744_1280.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2018/01/05/02/47/fishing-3062034_1280.jpg",
+  },
+]);
 </script>
 
 
@@ -147,9 +264,13 @@ const items = ref(
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(6, 6, 6, 0.568) 100%);
+  width: 110%;
+  height: 110%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.5) 0%,
+    rgba(6, 6, 6, 0.568) 100%
+  );
   z-index: 1000;
 }
 
@@ -161,5 +282,4 @@ const items = ref(
   text-align: center;
   color: white;
 }
-
 </style>
